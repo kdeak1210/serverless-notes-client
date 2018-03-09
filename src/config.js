3413b95279c05c25b20712bed93259bc5461bf92
kyle <kdeak1210@gmail.com>
2018-03-09 11:19:36 -0500

    Using the amazon-cognito-identity-js module to send login requests to our Cognito User Pool

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+  cognito: {
+    USER_POOL_ID: 'us-east-2_3VEmrQ9RJ',
+    APP_CLIENT_ID: '4cj5i96ge1e4uj540c2re3d6h1'
+  }
+};
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import { 
+  CognitoUserPool, 
+  AuthenticationDetails, 
+  CognitoUser 
+} from 'amazon-cognito-identity-js';
 import './Login.css';
+import config from '../config';
 
 export default class Login extends Component {
   state = {
@@ -19,8 +25,34 @@ export default class Login extends Component {
     });
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
+
+    try {
+      const { email, password } = this.state;
+      await this.login(email, password);
+      alert("Logged in!");
+    } catch (e) {
+      alert(JSON.stringify(e));
+    }
+  }
+
+  login = (email, password) => {
+    //console.log(email, password)
+    const userPool = new CognitoUserPool({
+      UserPoolId: config.cognito.USER_POOL_ID,
+      ClientId: config.cognito.APP_CLIENT_ID
+    });
+    const user = new CognitoUser({ Username: email, Pool: userPool });
+    const authenticationData = { Username: email, Password: password };
+    const authenticationDetails = new AuthenticationDetails(authenticationData);
+
+    return new Promise((resolve, reject) => 
+      user.authenticateUser(authenticationDetails, {
+        onSuccess: result => resolve(),
+        onFailure: err => reject(err)
+      })
+    );
   }
 
   render() {
