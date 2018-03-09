8eb9acb629ad626cae457cd43046387dcb673ecb
kyle <kdeak1210@gmail.com>
2018-03-09 10:49:56 -0500

    Created a Login form component to conditionally render at the /login route
    This form has controlled inputs, linked directly to the components local state. An onChange handler updates the state as values are typed into the form, and the updating of the state causes the controlled component to rerender. Styles are added to roughly target non-mobile viewports with a media query

---
+++ src/containers/Login.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import './Login.css';
+
+export default class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  validateForm = () => {
+    const { email, password } = this.state;
+    return email.length > 0 && password.length > 0;
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  render() {
+    return(
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl 
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl 
+              type="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
